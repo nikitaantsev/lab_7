5b8c9c2791451f53c27286ead592eb29083f0843
nikitaantsev <71016018+nikitaantsev@users.noreply.github.com>
2021-12-17 14:32:48 +0300

    Add files via upload

---
+++ lab_7/lab_7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab_7
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string firstName, string lastName, int salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Fitst Name: {0}. Last Name: {1}. Salary: {2}", FirstName, LastName, Salary);
+        }
+    }
+
+    class Company
+    {
+        public List<Employee> Employees { get; set; }
+
+        public void RecruitEmployee(string fisrtName, string lastName, int salary)
+        {
+            Employees.Add(new Employee(fisrtName, lastName, salary));
+        }
+
+        public void RecruitEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+        }
+
+        public void FireEmployee(string fisrtName, string lastName)
+        {
+            var employee = Employees.Find(e => e.FirstName.Equals(fisrtName) && e.LastName.Equals(lastName));
+            if (employee != null)
+            {
+                Employees.Remove(employee);
+            }
+
+        }
+
+        public string PrintEmployeesList()
+        {
+            return Employees.Aggregate("", (current, item) => current + item.ToString() + "\n");
+        }
+
+        public int GetAverageSalary()
+        {
+            return Employees.Sum(e => e.Salary) / Employees.Count;
+        }
+    }
+}
